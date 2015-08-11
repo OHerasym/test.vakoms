51ea5a49ddc08c82072619758610b9d7ca6bc568
Oleh Gerasum <oolleehh@gmail.com>
2015-08-11 19:50:38 +0300

    th

--- exuser/settings.py
+++ exuser/settings.py
@@ -16,7 +16,9 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config()
+}
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
